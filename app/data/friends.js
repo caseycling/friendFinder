97030df94a867937a349bb5438524d125fb826d8
Casey <caseyclinga@gmail.com>
2019-05-21 16:18:21 -0700

    Changed survery.js name to survey.js and add variables in friends.js to store current user data, the eventual matched friend data and an array to hold potential friends objects

---
+++ app/data/friends.js
@@ -0,0 +1,7 @@
+//Variable that stores current user information
+var currentUser 
+//Array that stores all user objects
+var potentialFriends = [];
+
+//Most compatiable match, after point evaluation
+var matchedFriend 
--- server.js
+++ server.js
@@ -1,3 +1,12 @@
+//DO I NEED TO REQUIRE HTTP?
+//WHAT DO I DO AFTER STARTING UP SERVER? SET UP GET REQUESTS?
+//PREVIOUSLY WE ROUTED IN THE SERVER FILE. HOW DO I LINK TO THE GET REQUESTS IF THEY ARE IN A DIFFERENT FILE? USE A SCRIPT TAGE FOR HTML ROUTES?
+//HOW DO I PASS USER INFORMATION INTO ARRAYS IN FRIENDS.JS? CLICK HANDLERS? POST REQUESTS?
+//I DONT ENTIRELY UNDERSTAND GET AND POST REQUESTS. GET IS TO GET INFORMATION FROM USER REQUEST (EX. QUESTIONAIRE ANSWERS)?
+//POST IS TO SEND INFORMATION TO USER (EX. MATCHED FRIEND RESULTS)?
+
+
+
 //Require express
 var express = require("express")
 //Require path
@@ -6,5 +15,8 @@ var path = require("path")
 var app = express()
 //Save port to variable
 var port = 8000
-//Use middleware to parse data passed to array from survey
+
 //Start the server up with the listen function
+app.listen(port);
+
+//Use middleware to parse data passed to array from survey
