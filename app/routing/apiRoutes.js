e59f2924736e13bbf31bf2953b6a03f229b1f4ff
Casey <caseyclinga@gmail.com>
2019-05-22 22:11:24 -0700

    Created router module and exported routes from htmlRoutes.js and apiRoutes.js

--- app/routing/apiRoutes.js
+++ app/routing/apiRoutes.js
@@ -1,14 +1,19 @@
 //API ROUTES
 //===============================================
 
+//Use router to create router module
+var express = require('express')
+var router = express.Router()
+
 // GET request to display all potential friends in JSON format with /api/friends entered as end point in url
-app.get("/api/friends", function (req, res) {
+router.get("/api/friends", function (req, res) {
     return res.json(potentialFriends);
 });
 
 // POST request to receive data from client and push to potentialFriends array
-app.post("/api/waitlist", function (req, res) {
+router.post("/api/waitlist", function (req, res) {
     return potentialFriends.push(req.body);
 });
 
-
+//Export routes
+module.exports = router
--- app/routing/htmlRoutes.js
+++ app/routing/htmlRoutes.js
@@ -1,12 +1,19 @@
 // HTML ROUTES
 // =============================================================
 
+//Use router to create router module
+var express = require('express')
+var router = express.Router()
+
 // Basic route that sends the user first to the home page
-app.get("/", function (req, res) {
+router.get("/", function (req, res) {
     res.sendFile(path.join(__dirname, "home.html"));
-});
+})
 
 //Route that sends to survey page
-app.get("/survey", function(req, res) {
+router.get("/survey", function (req, res) {
     res.sendFile(path.join(__dirname, "survey.html"))
 })
+
+//Export routes
+module.exports = router
