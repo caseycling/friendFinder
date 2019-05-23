4c486af73c14bb8f05094a88ae2130072cfdcf04
Casey <caseyclinga@gmail.com>
2019-05-23 13:16:30 -0700

    Added basic html to home page and began logic to route after clicking 'go to survey' button

--- app/data/friends.js
+++ app/data/friends.js
@@ -5,3 +5,11 @@ var potentialFriends = [];
 
 //Most compatiable match, after point evaluation
 var matchedFriend 
+
+//Click handler to route user to survey
+$("#survey").on("click", function() {
+    
+})
+
+//Capture user input for name, photo and scores
+//Save to currentUser object
