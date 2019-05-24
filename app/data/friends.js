470431421131cb968949b33480e1bdc09d790dcc
Casey <caseyclinga@gmail.com>
2019-05-23 21:47:27 -0700

    adjusted conditionals

--- app/data/friends.js
+++ app/data/friends.js
@@ -1,19 +1,50 @@
-//Variable that stores current user information
-var currentUser
-//Array that stores all user objects
-var potentialFriends = [];
-
-//Most compatiable match, after point evaluation
-var matchedFriend
-
-//Click handler to route user to survey
-$("#survey").on("click", function () {
-    // //Route that sends to survey page
-    // router.get("/survey", function (req, res) {
-    //     res.sendFile(path.join(__dirname, "../public/survey.html"))
-    // })
-    console.log("hello world")
-})
-
-//Capture user input for name, photo and scores
-//Save to currentUser object
+module.exports = [
+    {
+        "name": "Ahmed",
+        "photo": "https://media.licdn.com/mpr/mpr/shrinknp_400_400/p/6/005/064/1bd/3435aa3.jpg",
+        "scores": [
+            5,
+            1,
+            4,
+            4,
+            5,
+            1,
+            2,
+            5,
+            4,
+            1
+        ]
+    },
+    {
+        "name": "Mike",
+        "photo": "https://media.licdn.com/mpr/mpr/shrinknp_400_400/p/6/005/064/1bd/3435aa3.jpg",
+        "scores": [
+            3,
+            2,
+            4,
+            3,
+            5,
+            5,
+            3,
+            1,
+            3,
+            4
+        ]
+    },
+    {
+        "name": "Sarah",
+        "photo": "https://media.licdn.com/mpr/mpr/shrinknp_400_400/p/6/005/064/1bd/3435aa3.jpg",
+        "scores": [
+            2,
+            2,
+            5,
+            4,
+            3,
+            1,
+            5,
+            5,
+            3,
+            2
+        ]
+    }
+]
