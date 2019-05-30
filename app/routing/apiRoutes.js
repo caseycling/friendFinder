435e96aa2434f2263a924d5b9f434114b1b9ae9e
Casey <caseyclinga@gmail.com>
2019-05-30 16:52:22 -0700

    Added hyperlink to friends API and git hub repo

--- app/routing/apiRoutes.js
+++ app/routing/apiRoutes.js
@@ -5,7 +5,7 @@
 var express = require('express')
 var path = require('path')
 var router = express.Router()
-var potentialFriends = require('../data/friends');
+var potentialFriends = require('../data/friends.js');
 
 // GET request to display all potential friends in JSON format with /api/friends entered as end point in url
 router.get("/api/friends", function (req, res) {
